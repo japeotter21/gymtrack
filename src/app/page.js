8095ad8de3ed3fd9f8a5fc4ea8cf689994079d8b
john potter <juanpotter7@gmail.com>
2023-07-24 03:39:09 -0400

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,111 @@
+"use client"
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
+import { Avatar, Paper } from '@mui/material'
 
 export default function Home() {
+  const [streak, setStreak] = useState(0)
+  const [day, setDay] = useState(0)
+  const [week, setWeek] = useState(['S', 'M', 'T', 'W', 'R', 'F', 'S'])
+
+  useEffect(()=>{
+    setDay(new Date().getDay())
+  },[])
+
+  useEffect(()=>{
+    if(day > 0)
+    {
+      const weekTemp = [...week]
+      const reorderWeek = [weekTemp.slice(day), weekTemp.slice(0,day)]
+      const newWeek = reorderWeek[0].concat(reorderWeek[1])
+      setWeek(newWeek)
+    }
+  },[day])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center pt-6 lg:pt-12 px-2 lg:p-12 gap-4">
+      <div className='flex items-center justify-between w-full gap-3 lg:w-1/2'>
+        <Avatar>JP</Avatar>
+        <div className='w-full border border-gray-300 rounded-lg bg-stone-50 px-4 py-2'>
+          <div className='text-sm'>Name</div>
+          <hr className='my-1'/>
+          <div className='text-sm'>Goal: 160 lbs, 10.5% BF</div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='w-full lg:w-1/2 flex gap-5 items-center'>
+        <div className={streak > 0 ? `text-green-500` : `text-red-500`}>🔥&nbsp;{streak}</div>
+        <div className='text-sm'>Best: 12</div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div className='flex'>
+          {week.map((day,id)=>
+            <div className='border border-gray-300 rounded-lg bg-stone-50 p-2' key={id}>
+                <div className='text-sm text-gray-400'>
+                {day}
+                </div>
+                <div className='text-sm'>
+                  { id % 4 === 0 ?
+                    <>Rest</>
+                  : id % 2 === 0 ?
+                    <>Back</>
+                  : id % 3 === 0 ?
+                    <>Legs</>
+                  : 
+                    <>Chest</>
+                  }
+                </div>
+            </div>
+          )}
+        </div>
+        <div className='w-full lg:w-1/2 flex-col gap-2 items-center border border-gray-300 rounded-lg bg-stone-50 px-4 py-2'>
+          <div className='flex justify-between items-center'>
+            <p className='font-semibold text-lg'>Today's Workout</p>
+            <button className='shadow-md py-1 px-4 rounded-full bg-green-700 hover:bg-opacity-70 text-white'>Start</button>    
+          </div>
+          <div>Back</div>
+          <div className='text-sm border border-gray-300 rounded-md px-2 py-1 mt-2'>
+            <table className='w-full'>
+              <thead>
+                <tr className='text-left'>
+                  <th className='p-1'>Exercise</th>
+                  <th className='p-1'>Weight</th>
+                  <th className='p-1'>Info</th>
+                </tr>
+              </thead>
+              <tbody className='divide-y'>
+                <tr>
+                  <td className='p-1'>Weighted Pullups</td>
+                  <td className='p-1'>6x35</td>
+                  <td className='p-1 text-gray-400'></td>
+                </tr>
+                <tr>
+                  <td className='p-1'>T-bar Row</td>
+                  <td className='p-1'>8x90</td>
+                  <td className='p-1 text-gray-500'></td>
+                </tr>
+                <tr>
+                  <td className='p-1'>Lat Pulldown</td>
+                  <td className='p-1'>10x120</td>
+                  <td className='p-1 text-gray-500'>Slow up</td>
+                </tr>
+                <tr>
+                  <td className='p-1'>DB Shrug</td>
+                  <td className='p-1'>12x65</td>
+                  <td className='p-1 text-gray-500'>10 sec hold</td>
+                </tr>
+                <tr>
+                  <td className='p-1'>Preacher Curl</td>
+                  <td className='p-1'>6x65</td>
+                  <td className='p-1 text-gray-500'></td>
+                </tr>
+                <tr>
+                  <td className='p-1'>Incline DB Curl</td>
+                  <td className='p-1'>12x20</td>
+                  <td className='p-1 text-gray-500'></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
     </main>
   )
 }
